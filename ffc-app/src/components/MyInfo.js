6768705159b0880d6110807c86217415d0ce4c28
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-07-19 22:55:25 -0400

    starting the fcc react course

---
+++ ffc-app/src/components/MyInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../App.css';
+
+// function App() {
+//   return (
+//     <ul>
+//       <li>
+//         thing one
+//       </li>
+//       <li>
+//         thing two
+//       </li>
+//       <li>
+//         thing three
+//       </li>
+//     </ul>
+//   );
+// }
+
+function MyInfo() {
+  return (
+    <div class="App-header">
+      <h1>Stéfan Martin</h1>
+      <p> here is a little information about me just kidding there is no information the only 
+        thing that I will tell you is that I am currently trying to learn react so that I can 
+        make a really nice website
+      </p>
+      <ol>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+      </ol>
+    </div>
+  )
+}
+export default MyInfo;
