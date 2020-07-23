012bfd1e5370e03c33af45e96fd9d9285c4879e0
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-07-23 19:07:54 -0400

    doing stuff

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -3,8 +3,13 @@ import React from 'react';
 function TodoItem(props) {
   return (
     <div className="todoItem">
-      <input type="checkbox" checked={props.todo.completed}/>
-      <label for="one">{props.todo.text}</label>
+      <input 
+        type="checkbox" 
+        checked={props.todo.completed} 
+        name={props.todo.id} 
+        onChange={() => console.log("changed")}
+      />
+      <label for={props.todo.id}>{props.todo.text}</label>
       <br></br>
     </div>
   );
