edfc8f796aa3b5766fbcb72df8eff76a155f2eeb
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-07-24 23:21:38 -0400

    more practice

--- practice-app/src/components/App.js
+++ practice-app/src/components/App.js
@@ -89,33 +89,64 @@ import jokesData from '../jokeData';
 //   }
 // }
 
+// class App extends React.Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       isLoggedIn: false
+//     }
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   handleClick() {
+//     this.setState(prevState => {
+//       return {
+//         isLoggedIn: !prevState.isLoggedIn
+//       }
+//     })
+//   }
+
+//   render() {
+//     let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+//     let loggedIn = this.state.isLoggedIn ? "LOGGED IN" : "LOGGED OUT"
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>{buttonText}</button>
+//         <h1>{loggedIn}</h1>
+//       </div>
+//     )
+//   }
+// }
+
+
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      isLoggedIn: false
+      isLoading: false, 
+      character: {}
     }
-    this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick() {
-    this.setState(prevState => {
-      return {
-        isLoggedIn: !prevState.isLoggedIn
-      }
-    })
+  componentDidMount() {
+    this.setState({isLoading: true})
+    fetch("https://swapi.dev/api/people/1")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          isLoading: false, 
+          character: data
+        })
+      })
   }
 
   render() {
-    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
-    let loggedIn = this.state.isLoggedIn ? "LOGGED IN" : "LOGGED OUT"
+    const text = this.state.isLoading ? "loading..." : this.state.character.name
     return (
       <div>
-        <button onClick={this.handleClick}>{buttonText}</button>
-        <h1>{loggedIn}</h1>
+        <h1>{text}</h1>
       </div>
     )
   }
 }
-
 export default App;
