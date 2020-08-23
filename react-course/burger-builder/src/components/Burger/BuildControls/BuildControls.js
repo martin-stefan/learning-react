547b347cdf344b7861fd7d6bfd4a659354920ffe
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-23 12:14:53 -0400

    some work

--- react-course/burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ react-course/burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import classes from './BuildControls.module.css'
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) => {
-  <div>
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
+    {controls.map(ctrl => (
+      <BuildControl 
+        key={ctrl.label} 
+        label={ctrl.label}
+
+        added={() => props.added(ctrl.type)}
+        removed={() => props.removed(ctrl.type)} 
+        disabled={props.disabled[ctrl.type]} />
+    ))}
+
+    <button 
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}>
     
+      ORDER NOW</button>
   </div>
-};
+);
 
 export default buildControls;
