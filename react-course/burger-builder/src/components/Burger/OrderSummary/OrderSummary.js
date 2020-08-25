27b837d0ed31254a11bfbe8b225e3a871a984476
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-25 02:00:52 -0400

    work

--- react-course/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ react-course/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(key => {
-      return <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {props.ingredients[key]}</li>
-    });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A burger with the following ingredients</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-      <p>Continue to checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
 
-export default orderSummary;
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(key => {
+        return <li key={key}><span style={{textTransform: 'capitalize'}}>{key}</span>: {this.props.ingredients[key]}</li>
+      });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A burger with the following ingredients</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+
+    );
+  }
+}
+
+export default OrderSummary;
--- react-course/burger-builder/src/components/Layout/Layout.js
+++ react-course/burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
+class Layout extends Component {
+  state = {
+    showDrawer: false
+  }
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+  closedHandler = () => {
+    this.setState({showDrawer: false})
+  }
 
-export default layout;
+  toggleHandler = () => {
+    this.setState((prev) => {
+      return {showDrawer: !prev.showDrawer};
+    });
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar toggle={this.toggleHandler}/>
+        <SideDrawer open={this.state.showDrawer} closed={this.closedHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+};
+
+export default Layout;
