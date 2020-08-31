d8526ce7a2ccb62a585aae4a9bd071187bf6f7e2
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-08-30 21:36:41 -0400

    some work

--- react-course/sectionNine/src/containers/Blog/Blog.js
+++ react-course/sectionNine/src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import './Blog.css';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
@@ -14,7 +14,7 @@ class Blog extends Component {
 							<header>
 								<nav>
 									<ul>
-										<li><NavLink to="/" exact>Home</NavLink></li>
+										<li><NavLink to="/" exact>Posts</NavLink></li>
 										<li><NavLink to={{
 											pathname:"/new-post"
 											}}>New Post</NavLink>
@@ -22,9 +22,11 @@ class Blog extends Component {
 									</ul>
 								</nav>
 							</header>
-							<Route path="/" exact component={Posts}/>
-							<Route path="/new-post" component={NewPost}/>
-							<Route path="/:id" exact component={FullPost}/>
+							<Switch>
+								<Route path="/" exact component={Posts}/>
+								<Route path="/new-post" component={NewPost}/>
+								<Route path="/:id" exact component={FullPost}/>
+							</Switch>
             </div>
         );
     }
