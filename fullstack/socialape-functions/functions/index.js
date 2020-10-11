52550f5d72f8c1ec3343a2711d07ec61892eaf80
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-11 14:54:55 -0400

    validation for signup

--- fullstack/socialape-functions/functions/index.js
+++ fullstack/socialape-functions/functions/index.js
@@ -63,15 +63,50 @@ app.post("/scream", (req, res) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = (string) => {
+  if (string.trim() === "") return true;
+  else return false;
+};
+
 // sign up route
 
 app.post("/signup", (req, res) => {
   const newUser = {
     email: req.body.email,
     password: req.body.password,
-    confirmpPassword: req.body.confirmpPassword,
+    confirmPassword: req.body.confirmPassword,
     handle: req.body.handle,
   };
+
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) {
+    errors.password = "Must not be empty";
+  }
+
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  if (isEmpty(newUser.handle)) {
+    errors.handle = "Must not be empty";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return res.status(400).json(errors);
+  }
 
   // validate data
   let token, userId;
@@ -111,5 +146,7 @@ app.post("/signup", (req, res) => {
       }
     });
 });
+
+
 
 exports.api = functions.https.onRequest(app);
