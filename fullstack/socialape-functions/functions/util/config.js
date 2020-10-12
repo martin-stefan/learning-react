045d48e547fb9e97f3498b1adc493e7ebb88ef55
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-10-12 11:28:12 -0400

    organizing documents

---
+++ fullstack/socialape-functions/functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  apiKey: "AIzaSyAadfAUa4vXqBNEHhIifhfTKpQPhb0v83w",
+  authDomain: "socialape-93bcf.firebaseapp.com",
+  databaseURL: "https://socialape-93bcf.firebaseio.com",
+  projectId: "socialape-93bcf",
+  storageBucket: "socialape-93bcf.appspot.com",
+  messagingSenderId: "332500994209",
+  appId: "1:332500994209:web:21d33bea71c8cbcb6cc8e0",
+  measurementId: "G-2QNRZERNZP",
+};
